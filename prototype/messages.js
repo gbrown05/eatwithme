c90676df3da4a4219825b860705701b40f7fe230
George Brown <gbrown3593@gmail.com>
2014-04-14 13:22:31 -0400

    added automation to chat

--- prototype/messages.js
+++ prototype/messages.js
@@ -1,20 +1,9 @@
-var george = 0;
-//george = true;
-
 function showMessage()
 {
 	var clear = document.getElementById("msg");
 	var message = document.getElementById("msg").value;
-	if(george == 0) {
-		message = "You: " + message;
-		george = 1;
-	} else if (george == 1) {
-		message = "George: " + message;
-		george = 2;
-	} else {
-		message = "Emily: " + message;
-		george = 0;
-	}
+	message = "You: " + message;
+
 	//Now, create paragraph that will be added
 	var paragraph = document.createElement("p");
 	var update = document.createTextNode(message);
@@ -25,4 +14,18 @@ function showMessage()
 	document.getElementById("status").appendChild(paragraph, 
 		document.getElementById("status").firstChild);
 	clear.value = "";
+	var x = setTimeout(replyMessage, 2000);
+}
+
+function replyMessage()
+{
+	var message = "Emily: Sure! Sounds good."
+	var paragraph = document.createElement("p");
+	var update = document.createTextNode(message);
+
+	paragraph.appendChild(update);
+
+	//Finally, add the update at the top of the "status" div
+	document.getElementById("status").appendChild(paragraph, 
+	document.getElementById("status").firstChild);
 }
